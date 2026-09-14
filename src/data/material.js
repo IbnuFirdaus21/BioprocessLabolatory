9093addac01c4e885db6390da50905320507cf0d
Ibnu Firdaus Rachman <92310760+IbnuFirdaus21@users.noreply.github.com>
2026-09-15 05:50:09 +0700

    apus typo

--- src/data/material.js
+++ src/data/material.js
@@ -59,7 +59,7 @@ export const materials = [
   {
     id: "mat-9",
     title: "Modul 9 - Bakteri Selulosa",
-    description: "PPenjelasan umum mengenai selulosa bakteri, bakteri penghasil selulosa (misal Acetobacter xylinum), proses pembentukan selulosa secara fermentasi.",
+    description: "Penjelasan umum mengenai selulosa bakteri, bakteri penghasil selulosa (misal Acetobacter xylinum), proses pembentukan selulosa secara fermentasi.",
     fileUrl: "https://drive.google.com/uc?export=download&id=1Rx8aHmGTD3Pr2TzfS_cb_ce-U5pcuH6O",
     videoUrl: "https://www.youtube.com/watch?v=xsw1ESdrdaM",
   },
